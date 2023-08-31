f06e1d8a8b3dcc5835e1e8d84d86dd8744cba7e6
Greeny <117624471+Greenydon198@users.noreply.github.com>
2023-08-31 23:12:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+let text = document.getElementById("text");
+let delay = document.getElementById("delay");
+let btn = document.getElementById("btn");
+let op = document.getElementById("output")
+btn.addEventListener("click",out);
+async function out(){
+	await new Promise((resolve)=>{
+		setTimeout(()=>{resolve()},Number(delay.value));
+		// resolve();
+		// console.log(text.value,delay.value,`${text.value}`);
+	});
+	op.innerText = text.value;
+}
